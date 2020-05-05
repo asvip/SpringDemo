57d5a15474ed1e8049371a0d64d1ace4512e1b59
Jenly <jenly1314@gmail.com>
2020-05-05 22:28:19 +0800

    Update controller6

--- src/main/kotlin/com/king/spring/demo/Controller.kt
+++ src/main/kotlin/com/king/spring/demo/Controller.kt
@@ -13,8 +13,7 @@ class Controller{
 
     @GetMapping("/getRequest")
     fun getRequest(@RequestParam username: String?): Result<User?>{
-        if(username == "king1234567"){
-
+        if(username == "king123456"){
             return Result(0,"success",User(username,null,null))
         }
         return Result(1,"fail",null)
@@ -22,8 +21,7 @@ class Controller{
 
     @PostMapping("/postRequest")
     fun postRequest(@RequestParam username: String?): Result<User?> {
-        if (username == "king1234567") {
-
+        if (username == "king123456") {
             return Result(0, "success", User(username, null, null))
         }
         return Result(1, "fail", null)
